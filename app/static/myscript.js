6db0e2a2c16993724df807b81539051d766bc922
emre <emrec.tin@gmail.com>
2017-08-11 11:58:17 +0300

    Gereksiz Kodlar silindi.

--- app/static/myscript.js
+++ app/static/myscript.js
@@ -7,17 +7,6 @@ Vue.component('chart', {
             'chartobj': null}
   },
 
-  // //Watch komutu izleme value degerini izleme yapar.
-  // //Valude herangibir değişliklik olduğunda degerleri yeniler
-  // 'watch': {
-  //   'value': function() {
-  //     console.log('ahoy!');
-  //     this.chartobj.data.datasets[0].data[0] = this.value;
-  //     this.chartobj.data.datasets[0].data[1] = 4095 - this.value;
-  //     this.chartobj.update();
-  //   }
-  // },
-
   'mounted': function(){
     //this.$el dısardaki divi gösteriyor.children ile icerdeki chart'a ulastik
     this.ctx = this.$el.children[0].getContext('2d');
@@ -56,8 +45,6 @@ var app = new Vue({
   'methods': {
   'loadDoc': function(){
     var xhttp = new XMLHttpRequest();
-    //Sekron olarak islemlerin gerceklenmesi icin biri bitmeden diğeri olmayan birşey üzerinden
-    //istek yapmaya calisiyor olabilir.Bu engellenmesi icin.
     application = this;
     xhttp.onreadystatechange = function(){
       if (this.readyState == 4 && this.status == 200){
@@ -73,20 +60,3 @@ var app = new Vue({
   }
 }
 });
-
-
-// function loadDoc(){
-//   var xhttp = new XMLHttpRequest();
-//   //Sekron olarak islem tamamlanmadan xhttp istegi daha tamamlanmayıp
-//   //daha oluşturulmuş olabilir.Sekron çalışılması içi gereklidir.
-//   xhttp.onreadystatechange = function(){
-//     if (this.readyState == 4 && this.status == 200){
-//       text = this.responseText;
-//       document.getElementById("demo").innerHTML = text;
-
-//     }
-//   };
-//   xhttp.open("GET","http://172.22.9.13/api/ao/1/read", true);
-//   xhttp.send();
-//   console.log("Status:OK");
-// }
